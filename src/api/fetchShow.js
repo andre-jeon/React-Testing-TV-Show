4ec9afc9a05b1be665977fedb46752005eeae4d3
Myeongwon André Jeon <andrejeon@Myeongwons-MBP.fios-router.home>
2020-08-18 11:01:49 -0400

    next day

--- src/api/fetchShow.js
+++ src/api/fetchShow.js
@@ -5,6 +5,6 @@ import axios from 'axios'
 export const fetchShow = () => {
     return axios
     .get('"https://api.tvmaze.com/singlesearch/shows?q=stranger-things&embed=episodes"')
-    .then(res => console.log(res.data)) // or res.data, however you want to set that up
+    .then(res => console.log(res)) // or res.data, however you want to set that up
     .catch(err => console.log(err))
   }
