e978c08bb38edc0dd7667048eb8b0943703ce28e
Myeongwon André Jeon <andrejeon@Myeongwons-MBP.fios-router.home>
2020-08-18 11:06:12 -0400

    used useEfefect to fetchShow to App

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
 import Dropdown from "react-dropdown";
 import parse from "html-react-parser";
 
@@ -17,11 +16,11 @@ export default function App() {
   const episodes = seasons[selectedSeason] || [];
 
   useEffect(() => {
-    fetchShow().then((res) => {
-      setShow(res.data)
-      setSeasons(formatSeasons(res.data._embedded.episodes))
-    })
-
+    fetchShow()
+      .then((res) => {
+        setShow(res.data)
+        // setSeasons(formatSeasons(res.data._embedded.episodes))
+      })
   }, []);
 
   const handleSelect = e => {
--- src/api/fetchShow.js
+++ src/api/fetchShow.js
@@ -1,10 +1,9 @@
-import React from 'react'
 import axios from 'axios'
 
 // fetchShow.js
 export const fetchShow = () => {
     return axios
-    .get('"https://api.tvmaze.com/singlesearch/shows?q=stranger-things&embed=episodes"')
-    .then(res => console.log(res)) // or res.data, however you want to set that up
-    .catch(err => console.log(err))
+    .get(
+        "https://api.tvmaze.com/singlesearch/shows?q=stranger-things&embed=episodes"
+    )
   }
